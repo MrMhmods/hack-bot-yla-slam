e326ce01af9bec2028348ce2829d06777f90d032
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-07-06 09:56:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}! , ${client.user.id} , ${client.guilds.size} `);
 });
 
-    client = new Discord.Client({disableEveryone: true})
-    console.log("muuuuuuuuute,");
 client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("585389959113080832");
 if(!mohamed) return;
